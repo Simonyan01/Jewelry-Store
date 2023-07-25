16a1bb888b88fefa33199d9b8be81a1fa01d5a4c
Simonyan <lyov.simonyan.01@gmail.com>
2023-07-25 21:42:32 +0400

    Success

--- src/Filter/Filter.jsx
+++ src/Filter/Filter.jsx
@@ -1,69 +1,79 @@
+import { useState } from 'react';
+import Slider from 'react-slider'
 import "./filter.css"
 
+
 export const Filter = () => {
+    const min = 0, max = 10000
+    const [value, setValue] = useState([min, max]);
 
-    const rangeInput = document.querySelectorAll(".input-range input"),
-        priceInput = document.querySelectorAll(".input-price input"),
-        progress = document.querySelectorAll(".slider .progress"),
-        priceGap = 1000
+    const handleChange = (val) => setValue(val);
 
-    rangeInput.forEach((input) => {
-        input.addEventListener("input", (e) => {
-            let minVal = parseInt(rangeInput[0].value),
-                maxVal = parseInt(rangeInput[1].value)
-
-            if (maxVal - minVal < priceGap) {
-                if (e.target.className === "min-range") {
-                    rangeInput[0].value = maxVal - priceGap
-                } else {
-                    rangeInput[1].value = maxVal + priceGap
-                }
-            } else {
-                priceInput[0].value = minVal
-                priceInput[1].value = maxVal
-                progress.style.left = (minVal / rangeInput[0].max) * 100 + "%"
-                progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%"
-            }
-        })
-    })
-
-    // priceInput.forEach((input) => {
-    //     input.addEventListener("input", (e) => {
-    //         let minVal = parseInt(priceInput[0].value),
-    //             maxVal = parseInt(priceInput[1].value)
-
-    //         if ((maxVal - minVal >= priceGap) && maxVal <= 10000) {
-    //             if (e.target.className === "min") {
-    //                 rangeInput[0].value = maxVal - priceGap
-    //                 progress.style.left = (minVal / rangeInput[0].max) * 100 + "%"
-    //             } else {
-    //                 rangeInput[1].value = maxVal + priceGap
-    //                 progress.style.right = (maxVal / rangeInput[1].max) * 100 + "%"
-    //             }
-    //         }
-    //     })
-    // })
     return (
         <div className="filter-container">
-            <span>Цена</span>
-            <div className="input-price">
-                <div className="field">
-                    <span>От</span>
-                    <input type="number" className="min" value={2500} />
+            <h2>Цена</h2>
+            <Slider
+                className='slider'
+                defaultValue={[min, max]}
+                minDistance={1000}
+                min={min}
+                max={max}
+                step={50}
+                value={value}
+                pearling={true}
+                onChange={handleChange}
+            />
+            <div className='view'>
+                <div className='field'>
+                    <label htmlFor="minPrice">От</label>
+                    <input
+                        type="number"
+                        className="minPrice"
+                        value={value[0]}
+                        onChange={(e) => handleChange([+e.target.value])}
+                    />
                 </div>
-                <div className="separator">-</div>
-                <div className="field">
-                    <span>До</span>
-                    <input type="number" className="max" value={7500} />
+                <div className='field'>
+                    <label htmlFor="maxPrice">До</label>
+                    <input
+                        type="number"
+                        className="maxPrice"
+                        value={value[1]}
+                        onChange={(e) => handleChange([+e.target.value])}
+                    />
                 </div>
             </div>
-            <div className="slider">
-                <div className="progress"></div>
-            </div>
-            <div className="input-range">
-                <input type="range" className="min-range" min={0} max={10000} value={2500} />
-                <input type="range" className="max-range" min={0} max={10000} value={7500} />
-
+            <h2>Цена производства</h2>
+            <Slider
+                className='slider'
+                defaultValue={[min, max]}
+                minDistance={1000}
+                min={min}
+                max={max}
+                step={50}
+                value={value}
+                pearling={true}
+                onChange={handleChange}
+            />
+            <div className='view'>
+                <div className='field'>
+                    <label htmlFor="minPrice">От</label>
+                    <input
+                        type="number"
+                        className="minPrice"
+                        value={value[0]}
+                        onChange={(e) => handleChange([+e.target.value])}
+                    />
+                </div>
+                <div className='field'>
+                    <label htmlFor="maxPrice">До</label>
+                    <input
+                        type="number"
+                        className="maxPrice"
+                        value={value[1]}
+                        onChange={(e) => handleChange([+e.target.value])}
+                    />
+                </div>
             </div>
         </div>
     )
