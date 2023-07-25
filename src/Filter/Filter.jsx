8c268f5dc2ce8701f150b4e6b60ff483823cbc8b
Simonyan01 <lyova.simonyan.01@mail.ru>
2023-07-25 23:20:22 +0400

    Success

--- src/Filter/Filter.jsx
+++ src/Filter/Filter.jsx
@@ -1,81 +1,84 @@
-import { useState } from 'react';
-import Slider from 'react-slider'
-import "./filter.css"
-
+import { useState } from "react";
+import Slider from "react-slider";
+import "./filter.css";
 
 export const Filter = () => {
-    const min = 0, max = 10000
-    const [value, setValue] = useState([min, max]);
+  const min = 0,
+    max = 10000;
+  const [value, setValue] = useState([min, max]);
 
-    const handleChange = (val) => setValue(val);
+  const handleChange = (val) => setValue(val);
 
-    return (
-        <div className="filter-container">
-            <h2>Цена</h2>
-            <Slider
-                className='slider'
-                defaultValue={[min, max]}
-                minDistance={1000}
-                min={min}
-                max={max}
-                step={50}
-                value={value}
-                pearling={true}
-                onChange={handleChange}
+  return (
+    <div className="filter-container">
+      <div className="slider-container">
+        <h2>Цена</h2>
+        <Slider
+          className="slider"
+          defaultValue={[min, max]}
+          minDistance={1000}
+          min={min}
+          max={max}
+          step={50}
+          value={value}
+          pearling={true}
+          onChange={handleChange}
+        />
+        <div className="view">
+          <div className="field">
+            <label htmlFor="minPrice">От</label>
+            <input
+              type="number"
+              className="minPrice"
+              value={value[0]}
+              onChange={(e) => handleChange([+e.target.value])}
             />
-            <div className='view'>
-                <div className='field'>
-                    <label htmlFor="minPrice">От</label>
-                    <input
-                        type="number"
-                        className="minPrice"
-                        value={value[0]}
-                        onChange={(e) => handleChange([+e.target.value])}
-                    />
-                </div>
-                <div className='field'>
-                    <label htmlFor="maxPrice">До</label>
-                    <input
-                        type="number"
-                        className="maxPrice"
-                        value={value[1]}
-                        onChange={(e) => handleChange([+e.target.value])}
-                    />
-                </div>
-            </div>
-            <h2>Цена производства</h2>
-            <Slider
-                className='slider'
-                defaultValue={[min, max]}
-                minDistance={1000}
-                min={min}
-                max={max}
-                step={50}
-                value={value}
-                pearling={true}
-                onChange={handleChange}
+          </div>
+          <div className="field">
+            <label htmlFor="maxPrice">До</label>
+            <input
+              type="number"
+              className="maxPrice"
+              value={value[1]}
+              onChange={(e) => handleChange([+e.target.value])}
             />
-            <div className='view'>
-                <div className='field'>
-                    <label htmlFor="minPrice">От</label>
-                    <input
-                        type="number"
-                        className="minPrice"
-                        value={value[0]}
-                        onChange={(e) => handleChange([+e.target.value])}
-                    />
-                </div>
-                <div className='field'>
-                    <label htmlFor="maxPrice">До</label>
-                    <input
-                        type="number"
-                        className="maxPrice"
-                        value={value[1]}
-                        onChange={(e) => handleChange([+e.target.value])}
-                    />
-                </div>
-            </div>
+          </div>
         </div>
-    )
-}
-
+      </div>
+      <div className="slider-container">
+        <h2>Цена производства</h2>
+        <Slider
+          className="slider"
+          defaultValue={[min, max]}
+          minDistance={1000}
+          min={min}
+          max={max}
+          step={50}
+          value={value}
+          pearling={true}
+          onChange={handleChange}
+        />
+        <div className="view">
+          <div className="field">
+            <label htmlFor="minPrice">От</label>
+            <input
+              type="number"
+              className="minPrice"
+              value={value[0]}
+              onChange={(e) => handleChange([+e.target.value])}
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="maxPrice">До</label>
+            <input
+              type="number"
+              className="maxPrice"
+              value={value[1]}
+              onChange={(e) => handleChange([+e.target.value])}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
