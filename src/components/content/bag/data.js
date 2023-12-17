c4143032ac54a9393fddf49d2d36c59826e44ec0
Simonyan01 <lyov.simonyan.01@gmail.com>
2023-12-17 17:19:07 +0400

    Succeeded

---
+++ src/components/content/bag/data.js
@@ -0,0 +1,17 @@
+export const icons = [
+  {
+    id: 1,
+    img: "src/images/bag/group-80.svg",
+    title: "half",
+  },
+  {
+    id: 1,
+    img: "src/images/bag/ellipse-62.svg",
+    title: "full",
+  },
+  {
+    id: 1,
+    img: "src/images/bag/ellipse-63.svg",
+    title: "empty",
+  },
+]
---
+++ src/features/bag/BagSlice.jsx
@@ -0,0 +1,114 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const bagURL = "http://localhost:8080/bag"
+
+// Selected States
+
+export const selectBagState = state => state.bag
+
+// GET METHOD
+
+export const getBagList = createAsyncThunk(
+    "bag/getBagList",
+    async (_, thunkAPI) => {
+        try {
+            const res = await axios.get(bagURL)
+            return res?.data
+        } catch (err) {
+            return thunkAPI.rejectWithValue(err.message || "Failed to get bag list")
+        }
+    }
+)
+
+// POST METHOD
+
+export const postToBagList = createAsyncThunk(
+    "bag/postBagList",
+    async (req, thunkAPI) => {
+        try {
+            const res = await axios.post(bagURL, req)
+            return res?.data
+        } catch (err) {
+            return thunkAPI.rejectWithValue(err.message || "Failed to post bag list")
+        }
+    }
+)
+
+// DELETE METHOD
+
+export const deleteInBagList = createAsyncThunk(
+    "Bag/postCardToBag",
+    async (id, thunkAPI) => {
+        try {
+            const res = await axios.delete(`${bagURL}/${id}`)
+            return res?.data
+        } catch (err) {
+            return thunkAPI.rejectWithValue(
+                err.message || "Failed to delete bag list"
+            )
+        }
+    }
+)
+
+const initialState = {
+    loading: false,
+    error: null,
+    value: 1,
+    bag: [],
+}
+
+const bagSlice = createSlice({
+    name: "bag list",
+    initialState,
+    reducers: {
+        decrement: (state) => {
+            if (state.value > 1) {
+                state.value -= 1;
+            }
+        },
+        increment: (state) => {
+            state.value += 1
+        }
+    },
+    extraReducers: (builder) => {
+        // GET
+        builder
+            .addCase(getBagList.pending, (state) => {
+                state.loading = true
+            })
+            .addCase(getBagList.fulfilled, (state, action) => {
+                state.bag = action.payload;
+                state.loading = false;
+                state.error = null
+            })
+            .addCase(getBagList.rejected, (state, action) => {
+                state.error = action.error.message;
+                state.loading = false;
+            })
+            // DELETE
+            .addCase(deleteInBagList.pending, (state) => {
+                state.loading = true
+            })
+            .addCase(deleteInBagList.fulfilled, (state, action) => {
+                state.bag = state.bag.filter(item => item.id !== action.payload);
+                state.loading = false;
+                state.error = null
+            })
+            .addCase(deleteInBagList.rejected, (state, action) => {
+                state.error = action.error.message;
+                state.loading = false;
+            })
+        // COUNTER
+        // .addCase(increment, (state, action) => {
+        //     state.bag = action.payload
+        // })
+        // .addCase(decrement, (state, action) => {
+        //     state.bag = action.payload
+        // });
+    },
+})
+
+export const { increment, decrement } = bagSlice.actions
+
+export default bagSlice.reducer
